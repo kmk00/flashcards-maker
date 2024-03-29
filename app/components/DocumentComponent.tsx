743eb77ee793b3b3745b203cfee99dc9606b50d2
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-29 16:35:50 +0100

    Add downloading pdf on mobile

--- app/components/DocumentComponent.tsx
+++ app/components/DocumentComponent.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 import FlashcardsDocument from "./FlashcardsDocument";
 import { useFlashcardsStore } from "@/store/flashcards";
+import { usePDFOptions } from "@/store/options";
 
 const PDFDownloadLink = dynamic(
   () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
@@ -14,12 +15,24 @@ const PDFDownloadLink = dynamic(
 
 const DocumentComponent = () => {
   const { flashcards } = useFlashcardsStore();
+  const { currentOptions } = usePDFOptions();
 
   return (
     <div className="fixed lg:hidden bottom-0 w-full bg-slate-700 text-center py-2 hover:border-2 border-slate-500 cursor-pointer max-w-2xl right-[50%] translate-x-[50%]">
       <PDFDownloadLink
         // TODO: FIX Downloading PDF
-        document={<FlashcardsDocument flashcards={flashcards} />}
+        document={
+          <FlashcardsDocument
+            questionColor={currentOptions.questionColor}
+            answerColor={currentOptions.answerColor}
+            questionFontSize={currentOptions.questionFontSize}
+            answerFontSize={currentOptions.answerFontSize}
+            width={currentOptions.width}
+            height={currentOptions.height}
+            mode={currentOptions.mode}
+            flashcards={flashcards}
+          />
+        }
       >
         Download PDF
       </PDFDownloadLink>
