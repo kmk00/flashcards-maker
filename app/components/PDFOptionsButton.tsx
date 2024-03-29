dc7f6404ccb340db0aedc98abdcdddf8fa15e962
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-29 18:15:51 +0100

    Add smooth scrolling

--- app/components/PDFOptionsButton.tsx
+++ app/components/PDFOptionsButton.tsx
@@ -6,12 +6,17 @@ import Image from "next/image";
 const PDFOptionsButton = () => {
   const { displayOptions, showOptions } = usePDFOptions();
 
+  const handleButtonClick = () => {
+    displayOptions();
+    window.scrollTo(0, 0);
+  };
+
   return (
     <>
       {!showOptions && (
         <button
           className="fixed lg:bottom-5 bottom-10 right-5 text-center"
-          onClick={displayOptions}
+          onClick={handleButtonClick}
         >
           <Image
             className="cursor-pointer hover:animate-spin"
