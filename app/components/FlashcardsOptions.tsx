dfe23225070d9b2bc2a598596fdc9605865bf4f8
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-29 18:15:40 +0100

    Fix layout order

--- app/components/FlashcardsOptions.tsx
+++ app/components/FlashcardsOptions.tsx
@@ -2,7 +2,7 @@ import Flashcards from "./Flashcards";
 
 const FlashcardsOptions = () => {
   return (
-    <div className="flex flex-col justify-center lg:justify-start items-center lg:min-w-[500px] min-w-full  px-4 lg:overflow-y-auto pb-10">
+    <div className="flex flex-col order-2 lg:order-1 justify-center lg:justify-start items-center lg:min-w-[500px] min-w-full  px-4 lg:overflow-y-auto pb-10">
       <Flashcards />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ExamplePage from "./components/ExamplePage";
 
 export default function Home() {
   return (
-    <main className="flex-1 flex lg:ml-4 h-full justify-center items-center">
+    <main className="flex-1 order-1 lg:order-2 flex lg:ml-4 h-full justify-center items-center">
       <ExamplePage />
     </main>
   );
